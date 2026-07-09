1abc4c254b7f95b33ed9996952e83af75e48e632
Franklin Shera <fshera96@gmail.com>
2026-07-09 12:59:58 +0300

    chore: add demo content

--- playground/astro.config.mjs
+++ playground/astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import { assetMap } from "astro-asset-map";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [assetMap()],
+});
